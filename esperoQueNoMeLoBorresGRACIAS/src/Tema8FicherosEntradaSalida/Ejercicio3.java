fc117cc6aa64dbcefb8ff26938d8b5e61d87f754
Javier <jsanzdelcerro@iesch.org>
2025-12-17 17:21:21 +0100

    Subido

--- esperoQueNoMeLoBorresGRACIAS/src/Tema8FicherosEntradaSalida/Ejercicio3.java
+++ esperoQueNoMeLoBorresGRACIAS/src/Tema8FicherosEntradaSalida/Ejercicio3.java
@@ -1,5 +1,6 @@
 package Tema8FicherosEntradaSalida;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -7,10 +8,14 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 public class Ejercicio3 {
+
     public static void main(String[] args) throws IOException {
-        Path origen = Paths.get("texto.txt");
-        Path destino1 = Paths.get("texto1.txt");
-        Path destino2 = Paths.get("texto2.txt");
+        Path rutaAbs = Paths.get("C:\\Users\\dam1\\Desktop\\Nueva carpeta\\DAM1\\esperoQueNoMeLoBorresGRACIAS\\src\\docs");
+
+        Path origen = Paths.get(rutaAbs + "\\texto.txt");
+        Path destino1 = Paths.get(rutaAbs + "\\texto(+1).txt");
+        Path destino2 = Paths.get(rutaAbs + "\\texto(+2).txt");
+
 
         try {
             Files.copy(origen, destino1, StandardCopyOption.REPLACE_EXISTING);
@@ -19,7 +24,7 @@ public class Ejercicio3 {
             throw new RuntimeException(e);
         }
 
-        Path rutaNueva = Files.createDirectories(Path.of("Documentos/programación/docs/samples"));
+        Path rutaNueva = Files.createDirectories(Path.of(rutaAbs + "\\docs"));
         Path rutaNueva1 = Files.createDirectories(Path.of("Documentos/programación/docs/exercises"));
 
         
